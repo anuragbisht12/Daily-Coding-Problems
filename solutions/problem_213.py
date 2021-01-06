440dcf7399816970fb88b9a93c68527f993e0dc5
ANURAG BISHT <anuragbisht@ANURAGs-MacBook-Air.local>
2021-01-06 19:55:16 +0530

    mycommit

--- solutions/problem_213.py
+++ solutions/problem_213.py
@@ -31,3 +31,60 @@ assert generate_valid_ip_helper("255255255255") == \
     {'255.255.255.255'}
 assert generate_valid_ip_helper("100100110") == \
     {'100.10.0.110', '10.0.100.110', '100.100.11.0', '100.100.1.10'}
+
+
+"""
+New way
+"""
+
+"""
+string: 1921680
+O/P: [
+    1.9.216.80
+    1.92.16.80
+    1.92.168.0
+    19.2.168.0
+    19.21.6.80
+    19.21.68.0
+    19.216.8.0
+    192.1.6.80
+    192.1.68.0
+    192.16.8.0
+
+]
+
+"""
+
+# O(1) time and O(1) space
+def validIPAddresses(string):
+    # Write your code here.
+	ipAddressesFound=[]
+	
+	for i in range(1,min(len(string),4)):
+		currentIPAddressParts=["","","",""]
+		
+		currentIPAddressParts[0]=string[:i]
+		if not isValidPart(currentIPAddressParts[0]):
+			continue
+			
+		for j in range(i+1,i+min(len(string)-i,4)):
+			currentIPAddressParts[1]=string[i:j]
+			if not isValidPart(currentIPAddressParts[1]):
+				continue
+				
+			for k in range(j+1,j+min(len(string)-j,4)):
+				currentIPAddressParts[2]=string[j:k]
+				currentIPAddressParts[3]=string[k:]
+				
+				if isValidPart(currentIPAddressParts[2]) and isValidPart(currentIPAddressParts[3]):
+					ipAddressesFound.append(".".join(currentIPAddressParts))
+	
+    return ipAddressesFound
+
+def isValidPart(string):
+	stringAsInt=int(string)
+	if stringAsInt>255:
+		return False
+	
+	return len(string)==len(str(stringAsInt)) #check for leading zeros
+
