ff6d758bd070d3b4d1bc46980dc34b4a2efedd55
ANURAG BISHT <anuragbisht@ANURAGs-MacBook-Air.local>
2021-01-07 22:40:08 +0530

    mycommit

--- solutions/problem_192.py
+++ solutions/problem_192.py
@@ -10,3 +10,19 @@ def end_reachable(arr):
 # Tests
 assert end_reachable([1, 3, 1, 2, 0, 1])
 assert not end_reachable([1, 2, 1, 0, 0])
+
+
+
+def canJump(nums):
+	n = len(nums)
+	maxReached = 0
+	for curr in range(n):
+		jumpVal = nums[curr]
+		maxReached = max(maxReached, jumpVal+curr)  # update best record
+		if maxReached == curr:                      # reached max distance pottential
+			break
+	return maxReached >= n-1
+
+
+assert canJump([1, 3, 1, 2, 0, 1])
+assert not canJump([1, 2, 1, 0, 0])
